1b47f718dfc8b98cbda977950e00b7035b05fe84
dakorzhev <tasty0brains@gmail.com>
2019-05-29 00:59:09 +0300

    1.0.11

--- formats/image.js
+++ formats/image.js
@@ -11,9 +11,10 @@ const ATTRIBUTES = [
 
 class Image extends Parchment.Embed {
   static create(value) {
-    let node = super.create(value);
-    if (typeof value === 'string') {
-      node.setAttribute('src', this.sanitize(value));
+    let node = super.create(value.src);
+    if (typeof value.src === 'string') {
+      node.setAttribute('src', this.sanitize(value.src));
+      node.setAttribute('data-fid', value['data-fid'])
     }
     return node;
   }
